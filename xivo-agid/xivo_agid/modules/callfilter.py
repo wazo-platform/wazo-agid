5337d99e268e14d99694edb76e7b89de1e31887b
Cedric Abunar <cabunar@avencall.com>
2013-01-28 08:53:07 -0500

    fix check if caller user id is allowed

--- xivo-agid/xivo_agid/modules/callfilter.py
+++ xivo-agid/xivo_agid/modules/callfilter.py
@@ -20,12 +20,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from xivo_agid import agid
+from xivo_agid import agid, dialplan_variables
 from xivo_dao import callfilter_dao
 
 
 def callfilter(agi, cursor, args):
     callfiltermember_id = args[0]
+    caller_user_id = agi.get_variable(dialplan_variables.USERID)
     callfiltermember = callfilter_dao.get_by_callfiltermember_id(callfiltermember_id)
     if not callfiltermember:
         agi.dp_break('This callfilter does not exist.')
@@ -39,7 +40,7 @@ def callfilter(agi, cursor, args):
         callfilter, callfiltermembers = bs
         allow_ids.append(callfiltermembers.id)
 
-    if int(callfiltermember.id) not in allow_ids:
+    if not caller_user_id or int(caller_user_id) not in allow_ids:
         agi.dp_break('This user is not allowed to use this callfilter.')
 
     new_state = 0 if callfiltermember.active == 1 else 1
