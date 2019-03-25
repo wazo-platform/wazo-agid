cf120cfa6dd83141a5bde6536065027ba535dd04
Sebastien Duthil <sduthil@wazo.community>
2019-03-25 13:15:41 -0400

    group members: fix unit-tests

--- xivo_agid/modules/tests/test_group_member.py
+++ xivo_agid/modules/tests/test_group_member.py
@@ -6,23 +6,17 @@ import unittest
 
 from hamcrest import (
     assert_that,
-    calling,
     contains,
-    empty,
 )
 from mock import Mock
-from requests import HTTPError
 from xivo_agid.fastagi import FastAGI
-from xivo_test_helpers.hamcrest.raises import raises
 
-from ..group_member_add_remove import (
-    _get_sibling_interfaces,
-    UnknownLine,
-    UnknownUser,
+from ..group_member import (
+    _get_user_interfaces,
 )
 
 
-class TestGetSiblingInterfaces(unittest.TestCase):
+class TestGetUserInterfaces(unittest.TestCase):
 
     def setUp(self):
         self.agi = Mock(FastAGI)
@@ -44,51 +38,8 @@ class TestGetSiblingInterfaces(unittest.TestCase):
         }
         self.agi.get_variable.return_value = 'PJSIP/test'
 
-    def test_get_sibling_interfaces_unknown_line(self):
-        line_id = 12
-        self.confd_client.lines.get.side_effect = HTTPError(response=Mock(status_code=404))
-
-        assert_that(calling(_get_sibling_interfaces).with_args(self.agi, line_id),
-                    raises(UnknownLine))
-
-    def test_get_sibling_interfaces_line_has_no_extension(self):
-        line_id = 12
-        self.confd_client.lines.get.return_value = {
-            'name': 'test',
-            'extensions': [],
-            'users': []
-        }
-
-        result = _get_sibling_interfaces(self.agi, line_id)
-
-        assert_that(result, empty())
-
-    def test_get_sibling_interfaces_line_has_no_user(self):
-        line_id = 12
-        self.confd_client.lines.get.return_value = {
-            'name': 'test',
-            'extensions': [
-                {
-                    'exten': '1234',
-                    'context': 'default',
-                }
-            ],
-            'users': []
-        }
-
-        result = _get_sibling_interfaces(self.agi, line_id)
-
-        assert_that(result, empty())
-
-    def test_get_sibling_interfaces_unknown_user(self):
-        line_id = 12
-        self.confd_client.users.get.side_effect = HTTPError(response=Mock(status_code=404))
-
-        assert_that(calling(_get_sibling_interfaces).with_args(self.agi, line_id),
-                    raises(UnknownUser))
-
-    def test_get_sibling_interfaces(self):
-        line_id = 12
+    def test_get_user_interfaces(self):
+        user_uuid = 'abcdef'
         self.confd_client.users.get.return_value = {
             'lines': [
                 {
@@ -145,12 +96,12 @@ class TestGetSiblingInterfaces(unittest.TestCase):
         }
         self.agi.get_variable.return_value = 'PJSIP/sip-line'
 
-        result = _get_sibling_interfaces(self.agi, line_id)
+        result = _get_user_interfaces(self.agi, user_uuid)
 
-        assert_that(result, contains('PJSIP/sip-line', 'SCCP/sccp-line'))
+        assert_that(result, contains('PJSIP/sip-line', 'SCCP/sccp-line', 'SCCP/different-exten'))
 
-    def test_get_sibling_interfaces_pjsip_multicontact(self):
-        line_id = 12
+    def test_get_user_interfaces_pjsip_multicontact(self):
+        user_uuid = 'abcdef'
         self.confd_client.users.get.return_value = {
             'lines': [
                 {
@@ -170,6 +121,6 @@ class TestGetSiblingInterfaces(unittest.TestCase):
         }
         self.agi.get_variable.return_value = 'PJSIP/sip-line1&PJSIP/sip-line2'
 
-        result = _get_sibling_interfaces(self.agi, line_id)
+        result = _get_user_interfaces(self.agi, user_uuid)
 
         assert_that(result, contains('PJSIP/sip-line1', 'PJSIP/sip-line2'))
