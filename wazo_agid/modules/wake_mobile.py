b1c43914c82be0956cd2fa62be6383239a8c24ae
Sebastien Duthil <sduthil@wazo.community>
2021-08-18 16:06:23 -0400

    wake_mobile: get user uuid from args
    Why:
    
    * WAZO_DST_UUID is not set when calling from a group

--- wazo_agid/modules/wake_mobile.py
+++ wazo_agid/modules/wake_mobile.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019-2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_agid import agid
 
 
 def wake_mobile(agi, cursor, args):
+    user_uuid = args[0]
     should_wake_mobile = agi.get_variable('WAZO_WAIT_FOR_MOBILE') or False
     if not should_wake_mobile:
         return
 
-    user_uuid = agi.get_variable('WAZO_DST_UUID')
     video_enabled = agi.get_variable('WAZO_VIDEO_ENABLED')
     agi.appexec('UserEvent', 'Pushmobile,WAZO_DST_UUID: {},WAZO_VIDEO_ENABLED: {}'.format(user_uuid, video_enabled))
 
