c2f4c2281f9f582da829ed7fa0c2f95b3da04da9
Alexandre Fournier <afournier@wazo.io>
2023-02-13 16:32:14 -0500

    wake_mobile: add ring time to event

--- wazo_agid/modules/wake_mobile.py
+++ wazo_agid/modules/wake_mobile.py
@@ -10,13 +10,15 @@ from wazo_agid import agid
 def wake_mobile(agi: agid.FastAGI, cursor: DictCursor, args: list[str]) -> None:
     user_uuid = args[0]
     should_wake_mobile = agi.get_variable('WAZO_WAIT_FOR_MOBILE') or False
+
     if not should_wake_mobile:
         return
 
     video_enabled = agi.get_variable('WAZO_VIDEO_ENABLED')
+    ring_time = agi.get_variable('XIVO_RINGSECONDS') or 30
     agi.appexec(
         'UserEvent',
-        f'Pushmobile,WAZO_DST_UUID: {user_uuid},WAZO_VIDEO_ENABLED: {video_enabled}',
+        f'Pushmobile,WAZO_DST_UUID: {user_uuid},WAZO_VIDEO_ENABLED: {video_enabled},WAZO_RING_TIME: {ring_time}',
     )
 
 
