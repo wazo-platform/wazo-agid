2e4ff887964b5a6c4f9c9cb5352a77730dae4966
Charles Langlois <charles.langlois@wazo.io>
2024-04-17 14:05:12 -0400

    linear_group_check_timeout: use XIVO_GROUPTIMEOUT variable for group global timeout
    why: avoid unnecessary db queries(less resource intensive)

--- wazo_agid/modules/linear_group_check_timeout.py
+++ wazo_agid/modules/linear_group_check_timeout.py
@@ -6,9 +6,6 @@ import logging
 import time
 from typing import TYPE_CHECKING
 
-from xivo_dao.alchemy.groupfeatures import GroupFeatures
-from xivo_dao.resources.group import dao as group_dao
-
 from wazo_agid import agid
 
 if TYPE_CHECKING:
@@ -23,21 +20,24 @@ logger = logging.getLogger(__name__)
 def linear_group_check_timeout(
     agi: FastAGI, cursor: DictCursor, args: list[str]
 ) -> None:
-    group_id = args[0]
-    group: GroupFeatures = group_dao.get(group_id=group_id)
+    group_id = agi.get_variable('WAZO_DSTID')
 
-    if not group.timeout:
+    if not (_group_timeout := agi.get_variable('XIVO_GROUPTIMEOUT')):
+        logger.info('XIVO_GROUPTIMEOUT not set for group %s', group_id)
         return
+    else:
+        group_timeout = float(_group_timeout)
 
     current_time = time.time()
 
     if not (_start_time := agi.get_variable('WAZO_GROUP_START_TIME')):
         start_time = current_time
         agi.set_variable('WAZO_GROUP_START_TIME', str(start_time))
+        return
     else:
         start_time = float(_start_time)
 
-    if (current_time - start_time) >= group.timeout:
+    if (current_time - start_time) >= group_timeout:
         agi.set_variable('WAZO_GROUP_TIMEOUT_EXPIRED', '1')
 
 
