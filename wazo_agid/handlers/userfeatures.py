e62b9a60e0ef4d457370e7175329de9cc37e61bc
Alexandre Fournier <afournier@wazo.io>
2024-11-26 12:09:32 -0500

    userfeatures: rename set_simultcalls to set_dest_simultcalls
    Why: now there's a difference

--- wazo_agid/handlers/userfeatures.py
+++ wazo_agid/handlers/userfeatures.py
@@ -58,7 +58,7 @@ class UserFeatures(Handler):
             return
 
         self._set_options()
-        self._set_simultcalls()
+        self._set_dest_simultcalls()
         self._set_ringseconds()
         self._set_enablednd()
         self._set_mailbox()
@@ -411,7 +411,7 @@ class UserFeatures(Handler):
         else:
             self._agi.set_variable(name, '')
 
-    def _set_simultcalls(self):
+    def _set_dest_simultcalls(self):
         return self._agi.set_variable('WAZO_SIMULTCALLS', self._user.simultcalls)
 
     def _set_enablednd(self):
