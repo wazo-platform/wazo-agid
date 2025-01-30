b664f5f35ea75a97182217862795d8db4140d126
Pascal Cadotte Michaud <pcm@wazo.io>
2025-01-30 10:11:12 -0500

    rename XIVO_QUEUEPREPROCESS_SUBROUTINE

--- integration_tests/suite/test_handlers.py
+++ integration_tests/suite/test_handlers.py
@@ -1243,7 +1243,7 @@ def test_incoming_queue_set_features(base_asset: BaseAssetLaunchingHelper):
     assert recv_vars['XIVO_QUEUENEEDANSWER'] == '0'
     assert recv_vars[dv.QUEUEURL] == 'localhost'
     assert recv_vars['XIVO_QUEUEANNOUNCEOVERRIDE'] == 'override'
-    assert recv_vars['XIVO_QUEUEPREPROCESS_SUBROUTINE'] == 'subroutine'
+    assert recv_vars[dv.QUEUEPREPROCESS_SUBROUTINE] == 'subroutine'
     assert recv_vars[dv.QUEUETIMEOUT] == '25'
 
     assert recv_vars[dv.FWD_QUEUE_NOANSWER_ACTION] == 'queue'
