aeff309da662e8bde2144d799a1a149786e371d6
Alexandre Fournier <afournier@wazo.io>
2026-06-02 14:17:19 -0400

    CallerID: allow spaces in non-quoted caller ID

--- wazo_agid/tests/test_objects.py
+++ wazo_agid/tests/test_objects.py
@@ -1,4 +1,4 @@
-# Copyright 2023-2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2023-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import annotations
@@ -7,7 +7,7 @@ from unittest import TestCase
 
 from hamcrest import assert_that, is_
 
-from ..objects import VMBox
+from ..objects import CallerID, VMBox
 
 
 class VMBoxFastInit(VMBox):
@@ -43,3 +43,25 @@ class TestVMBox(TestCase):
         vmbox.skipcheckpass = 1
 
         assert_that(vmbox.has_password(), is_(False))
+
+
+class TestCallerID(TestCase):
+    def test_parse(self):
+        test_cases = (
+            ('Test <123>', ('Test', '123')),
+            ('"Test" <+123>', ('Test', '+123')),
+            ('"Test <+123>', None),
+            ('Test" <+123>', None),
+            ('"Test word   " <+123>', ('Test word   ', '+123')),
+            ('Test2 word    <+123>', ('Test2 word', '+123')),
+            ('Test3 word    <123>', ('Test3 word', '123')),
+            ('  Test4 word    <123>', ('Test4 word', '123')),
+            ('a', ('a', None)),
+            ('1', ('1', '1')),
+            ('+123', ('+123', '+123')),
+            ('anonymous', ('anonymous', None)),
+            ('', None),
+            (None, None),
+        )
+        for test_case, expected_result in test_cases:
+            assert CallerID.parse(test_case) == expected_result
